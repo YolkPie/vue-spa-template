be880b3dae6836520755649038e647cc0fb207bb
yujihu <yujihu@jd.com>
2020-01-10 15:17:38 +0800

    improvement: 1.完善readme；2.prettier配置。

--- vue.config.js
+++ vue.config.js
@@ -114,6 +114,7 @@ module.exports = {
   // 打包时不生成.map文件
   productionSourceMap: false,
   devServer: {
+    port: 80,
     open: true,
     disableHostCheck: true // 解决127.0.0.1指向其他域名时出现"Invalid Host header"问题
     // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
